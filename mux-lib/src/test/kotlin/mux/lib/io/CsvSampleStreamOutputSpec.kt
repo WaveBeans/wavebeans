cee791a36752bcb5b55e4152729193f435890d5d
asubb <a.a.subb@gmail.com>
2019-09-08 18:25:54 -0400

    [bugfix][output stream was joining different samples every step]

--- mux-lib/src/test/kotlin/mux/lib/io/CsvSampleStreamOutputSpec.kt
+++ mux-lib/src/test/kotlin/mux/lib/io/CsvSampleStreamOutputSpec.kt
@@ -2,9 +2,7 @@ package mux.lib.io
 
 import assertk.assertThat
 import assertk.assertions.*
-import mux.lib.RectangleWindow
-import mux.lib.stream
-import mux.lib.stream.fft
+import mux.lib.stream.minus
 import mux.lib.stream.sine
 import mux.lib.stream.trim
 import org.spekframework.spek2.Spek
@@ -21,6 +19,59 @@ class CsvSampleStreamOutputSpec : Spek({
     describe("A sinusoid of 10Hz, 500ms") {
         val sampleRate = 200.0f
         val x = 10.sine().trim(500)
+
+        describe("Writing to CSV with 100ms steps") {
+            val file = File.createTempFile("test_", ".mux.tmp")
+            CsvSampleStreamOutput(file.toURI(), TimeUnit.MILLISECONDS, x).use { csvOutput ->
+                csvOutput.writer(sampleRate).use {
+                    while (it.write(100, TimeUnit.MILLISECONDS)) {
+                    }
+                }
+            }
+
+            val expectedTimes = (0 until 100) // overall 100 samples
+                    .map { (it * 5).toDouble() } // 5 ms per sample
+
+            BufferedReader(InputStreamReader(FileInputStream(file))).use { reader ->
+                val lines = reader.lines().toList()
+
+                it("should have 101 lines") { assertThat(lines.size).isEqualTo(101) }
+
+                it("2+ lines, 2nd column should have double values of Signal") {
+                    assertThat(
+                            lines.drop(1)
+                                    .map { l ->
+                                        l.split(",")
+                                                .drop(1)
+                                                .map { it.toDoubleOrNull() }
+                                    }
+                    ).each { a ->
+                        a.isNotEmpty()
+                                .also { a.size().isEqualTo(1) }
+                                .also { a.each { it.isNotNull() } }
+                    }
+                }
+
+                it("2+ lines, 1st column should have time in ms") {
+                    assertThat(
+                            lines.drop(1)
+                                    .map { l ->
+                                        l.split(",")
+                                                .take(1)
+                                                .map { it.toDouble() }
+                                    }
+                                    .flatten()
+                                    .sorted()
+                    ).isEqualTo(expectedTimes)
+                }
+
+            }
+        }
+    }
+
+    describe("A diff of 2 sinusoids of 10Hz and 20 Hz, 500ms") {
+        val sampleRate = 200.0f
+        val x = (10.sine() - 20.sine()).trim(500)
 
         describe("Writing to CSV with 100ms steps") {
             val file = File.createTempFile("test_", ".mux.tmp")
