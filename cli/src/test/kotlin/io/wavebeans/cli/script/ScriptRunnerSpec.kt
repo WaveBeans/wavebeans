9e3aa5b811f2408445b84b16faf1f3f10e9cdd62
asubb <a.a.subb@gmail.com>
2019-12-15 14:03:32 -0500

    [script runner flaky test: script trying to stop before even attempting to launch on some machines]

--- cli/src/test/kotlin/io/wavebeans/cli/script/ScriptRunnerSpec.kt
+++ cli/src/test/kotlin/io/wavebeans/cli/script/ScriptRunnerSpec.kt
@@ -6,6 +6,7 @@ import assertk.catch
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.io.File
+import java.lang.Thread.sleep
 import java.util.concurrent.CancellationException
 
 object ScriptRunnerSpec : Spek({
@@ -23,20 +24,20 @@ object ScriptRunnerSpec : Spek({
 
             describe("Short-living script with one output") {
                 val script = """
-            440.sine().trim(1).toDevNull().out()
-        """.trimIndent()
+                    440.sine().trim(1).toDevNull().out()
+                """.trimIndent()
 
                 it("should run with no exceptions") { assertThat(eval(script)).isNull() }
             }
 
             describe("Short-living script with multiple outputs") {
                 val script = """
-            440.sine().trim(1).toDevNull().out()
-            880.sine().trim(1).toDevNull().out()
-            1760.sine().trim(1).toDevNull().out()
-            3520.sine().trim(1).toDevNull().out()
-            7040.sine().trim(1).toDevNull().out()
-        """.trimIndent()
+                    440.sine().trim(1).toDevNull().out()
+                    880.sine().trim(1).toDevNull().out()
+                    1760.sine().trim(1).toDevNull().out()
+                    3520.sine().trim(1).toDevNull().out()
+                    7040.sine().trim(1).toDevNull().out()
+                """.trimIndent()
 
                 it("should run with no exceptions") { assertThat(eval(script)).isNull() }
             }
@@ -44,11 +45,11 @@ object ScriptRunnerSpec : Spek({
             describe("Short-living script with more complicated logic") {
                 val file = File.createTempFile("test", ".csv").also { it.deleteOnExit() }
                 val script = """
-            val i1 = 440.sine()
-            val i2 = 880.sine()
-            
-            (i1 + i2).trim(1).toCsv("file://${file.absolutePath}").out()
-        """.trimIndent()
+                    val i1 = 440.sine()
+                    val i2 = 880.sine()
+                    
+                    (i1 + i2).trim(1).toCsv("file://${file.absolutePath}").out()
+                """.trimIndent()
 
                 it("should run with no exceptions") { assertThat(eval(script)).isNull() }
                 it("should generate non-empty file") { assertThat(file.readText()).isNotEmpty() }
@@ -57,12 +58,14 @@ object ScriptRunnerSpec : Spek({
             describe("Long-living script interruption") {
                 val file = File.createTempFile("test", ".csv").also { it.deleteOnExit() }
                 val script = """
-            440.sine().trim(Long.MAX_VALUE).toCsv("file://${file.absolutePath}").out() // takes forever to finish
-        """.trimIndent()
+                    440.sine().trim(Long.MAX_VALUE).toCsv("file://${file.absolutePath}").out() // takes forever to finish
+                """.trimIndent()
 
                 it("should return something after interruption") {
                     // This test uses sleeps, better to wait properly, test may become flaky
                     ScriptRunner(script, runMode = runMode, runOptions = runOptions).start().use { runner ->
+
+                        sleep(5000)
 
                         assertThat(runner.result(), "not finished right after the start")
                                 .prop("finished") { it.first }.isFalse()
@@ -85,8 +88,8 @@ object ScriptRunnerSpec : Spek({
 
             describe("Compile time error in script") {
                 val script = """
-            noSuchMethod()
-        """.trimIndent()
+                    noSuchMethod()
+                """.trimIndent()
 
                 it("should run with exception explaining the reason") {
                     assertThat(eval(script))
@@ -97,8 +100,8 @@ object ScriptRunnerSpec : Spek({
 
             describe("Runtime error in script") {
                 val script = """
-            throw Exception("my exception")
-        """.trimIndent()
+                    throw Exception("my exception")
+                """.trimIndent()
 
                 it("should run with exception explaining the reason") {
                     assertThat(eval(script))
@@ -110,10 +113,10 @@ object ScriptRunnerSpec : Spek({
             describe("Using imports") {
                 describe("one import") {
                     val script = """
-                import java.util.Date
-                
-                Date()
-            """.trimIndent()
+                        import java.util.Date
+                        
+                        Date()
+                    """.trimIndent()
 
                     it("should run with no exceptions") {
                         assertThat(eval(script)).isNull()
@@ -121,12 +124,12 @@ object ScriptRunnerSpec : Spek({
                 }
                 describe("several imports") {
                     val script = """
-                import java.util.Date
-                import kotlin.collections.*
-                
-                Date()
-                val a = emptyList<Int>()
-            """.trimIndent()
+                        import java.util.Date
+                        import kotlin.collections.*
+                        
+                        Date()
+                        val a = emptyList<Int>()
+                    """.trimIndent()
 
                     it("should run with no exceptions") {
                         assertThat(eval(script)).isNull()
@@ -135,11 +138,11 @@ object ScriptRunnerSpec : Spek({
 
                 describe("several imports in one line") {
                     val script = """
-                import java.util.Date; import kotlin.collections.*
-                
-                Date()
-                val a = emptyList<Int>()
-            """.trimIndent()
+                        import java.util.Date; import kotlin.collections.*
+                        
+                        Date()
+                        val a = emptyList<Int>()
+                    """.trimIndent()
 
                     it("should run with no exceptions") {
                         assertThat(eval(script)).isNull()
