530ccdd07946575bb7736fb7a89cce255782d1ae
asubb <a.a.subb@gmail.com>
2019-12-18 17:34:36 -0500

    [function input][input as function basic implementation]

---
+++ lib/src/main/kotlin/io/wavebeans/lib/io/FunctionInput.kt
@@ -0,0 +1,56 @@
+package io.wavebeans.lib.io
+
+import io.wavebeans.lib.BeanParams
+import io.wavebeans.lib.BeanStream
+import io.wavebeans.lib.SourceBean
+import kotlinx.serialization.*
+import kotlinx.serialization.internal.SerialClassDescImpl
+import kotlin.reflect.jvm.jvmName
+
+fun <T : Any> input(generator: (Long, Float) -> T?): BeanStream<T> = Input(InputParams(generator))
+
+object InputParamsSerializer : KSerializer<InputParams<*>> {
+
+    override val descriptor: SerialDescriptor = object : SerialClassDescImpl("InputParams") {
+        init {
+            addElement("generateFn")
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): InputParams<*> {
+        val dec = decoder.beginStructure(descriptor)
+        var funcClazzName: String? = null
+        loop@ while (true) {
+            when (val i = dec.decodeElementIndex(descriptor)) {
+                CompositeDecoder.READ_DONE -> break@loop
+                0 -> funcClazzName = dec.decodeStringElement(descriptor, i)
+                else -> throw SerializationException("Unknown index $i")
+            }
+        }
+        @Suppress("UNCHECKED_CAST") val funcByName = Class.forName(funcClazzName).newInstance() as (Long, Float) -> Any?
+        return InputParams(funcByName)
+    }
+
+    override fun serialize(encoder: Encoder, obj: InputParams<*>) {
+        val funcName = obj.generator::class.jvmName
+        val structure = encoder.beginStructure(descriptor)
+        structure.encodeStringElement(descriptor, 0, funcName)
+        structure.endStructure(descriptor)
+    }
+
+}
+
+@Serializable(with = InputParamsSerializer::class)
+class InputParams<T : Any>(val generator: (Long, Float) -> T?) : BeanParams()
+
+class Input<T : Any>(
+        override val parameters: InputParams<T>
+) : BeanStream<T>, SourceBean<T> {
+
+    override fun asSequence(sampleRate: Float): Sequence<T> =
+            (0..Long.MAX_VALUE).asSequence()
+                    .map { parameters.generator(it, sampleRate) }
+                    .takeWhile { it != null }
+                    .map { it!! }
+
+}
---
+++ lib/src/test/kotlin/io/wavebeans/lib/io/FunctionInputSpec.kt
@@ -0,0 +1,31 @@
+package io.wavebeans.lib.io
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import io.wavebeans.lib.asInt
+import io.wavebeans.lib.sampleOf
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object FunctionInputSpec : Spek({
+    describe("Sequence of integers") {
+
+        it("should generate 10 integers if it return only that") {
+            val seq = input { x, _ -> if (x < 10) sampleOf(x.toInt()) else null }
+                    .asSequence(44100.0f)
+                    .map { it.asInt() }
+                    .take(100)
+                    .toList()
+            assertThat(seq).isEqualTo((0..9).toList())
+        }
+
+        it("should generate 100 integers as input doesn't limit it") {
+            val seq = input { x, _ -> sampleOf(x.toInt()) }
+                    .asSequence(44100.0f)
+                    .map { it.asInt() }
+                    .take(100)
+                    .toList()
+            assertThat(seq).isEqualTo((0..99).toList())
+        }
+    }
+})
