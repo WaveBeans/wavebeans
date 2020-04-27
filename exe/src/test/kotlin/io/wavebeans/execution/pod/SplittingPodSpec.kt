86a9e187a2569d05f10a5c0dd3351070671341d7
asubb <a.a.subb@gmail.com>
2020-04-27 10:51:42 -0400

    [distributed execution][test fixes which were interfering with the rest via shared ExecutionConfig]

--- exe/src/test/kotlin/io/wavebeans/execution/pod/SplittingPodSpec.kt
+++ exe/src/test/kotlin/io/wavebeans/execution/pod/SplittingPodSpec.kt
@@ -10,6 +10,7 @@ import io.wavebeans.execution.newTestSplittingPod
 import io.wavebeans.lib.Sample
 import io.wavebeans.lib.asInt
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.lifecycle.CachingMode
 import org.spekframework.spek2.style.specification.describe
 
 object SplittingPodSpec : Spek({
@@ -25,30 +26,24 @@ object SplittingPodSpec : Spek({
 
     modes.forEach { mode ->
         describe("Mode: ${mode.key}") {
-            beforeGroup { mode.value.first() }
+            beforeEachGroup { mode.value.first() }
 
             val sampleRemap = mode.value.second
 
             describe("SplittingPod returning predefined sequence. Two partitions") {
 
                 describe("Iterate over pod sequence") {
-                    newTestSplittingPod(seq, 2).use { pod ->
-                        val iteratorKey0 = pod.iteratorStart(100.0f, 0)
-                        val iteratorKey1 = pod.iteratorStart(100.0f, 1)
+                    val pod by memoized { newTestSplittingPod(seq, 2) }
+                    val iteratorKeys by memoized { arrayOf(pod.iteratorStart(100.0f, 0), pod.iteratorStart(100.0f, 1)) }
 
-                        describe("Partition 0") {
-                            val result = sampleRemap(pod.iteratorNext(iteratorKey0, 50))
-                                    ?.map { it.asInt() }
+                    it("Partition 0: should be the same as defined sequence even elements") {
+                        assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[0], 50))?.map { it.asInt() })
+                                .isEqualTo(seq.windowed(2, 2).map { it[0] })
+                    }
 
-                            it("should be the same as defined sequence even elements") { assertThat(result).isEqualTo(seq.windowed(2, 2).map { it[0] }) }
-                        }
-
-                        describe("Partition 1") {
-                            val result = sampleRemap(pod.iteratorNext(iteratorKey1, 50))
-                                    ?.map { it.asInt() }
-
-                            it("should be the same as defined sequence odd elements") { assertThat(result).isEqualTo(seq.windowed(2, 2).map { it[1] }) }
-                        }
+                    it("Partition 1: should be the same as defined sequence odd elements") {
+                        assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[1], 50))?.map { it.asInt() })
+                                .isEqualTo(seq.windowed(2, 2).map { it[1] })
                     }
 
                 }
@@ -56,154 +51,115 @@ object SplittingPodSpec : Spek({
 
             describe("SplittingPod returning predefined sequence. One partition") {
                 describe("Iterate over pod sequence") {
-                    newTestSplittingPod(seq, 1).use { pod ->
+                    val pod by memoized { newTestSplittingPod(seq, 1) }
 
-                        val iteratorKey0 = pod.iteratorStart(100.0f, 0)
+                    val iteratorKey0 by memoized { pod.iteratorStart(100.0f, 0) }
 
-                        describe("Partition 0") {
-                            val result = sampleRemap(pod.iteratorNext(iteratorKey0, 100))
-                                    ?.map { it.asInt() }
-
-                            it("should be the same as defined sequence") { assertThat(result).isEqualTo(seq) }
-                        }
+                    it("Partition 0: should be the same as defined sequence") {
+                        assertThat(sampleRemap(pod.iteratorNext(iteratorKey0, 100))?.map { it.asInt() })
+                                .isEqualTo(seq)
                     }
-
                 }
             }
 
             describe("SplittingPod returning predefined sequence. Three partitions") {
                 describe("Iterate over pod sequence") {
-                    newTestSplittingPod(seq, 3).use { pod ->
+                    val pod by memoized(mode = CachingMode.SCOPE) { newTestSplittingPod(seq, 3) }
 
-                        val iteratorKey0 = pod.iteratorStart(100.0f, 0)
-                        val iteratorKey1 = pod.iteratorStart(100.0f, 1)
-                        val iteratorKey2 = pod.iteratorStart(100.0f, 2)
-
-                        describe("Partition 0") {
-                            val result = sampleRemap(pod.iteratorNext(iteratorKey0, 34))
-                                    ?.map { it.asInt() }
-
-                            it("should be the same as defined sequence 0th elements in triplets") {
-                                assertThat(result).isEqualTo(seq.windowed(3, 3, true).map { it[0] })
-                            }
-                        }
-                        describe("Partition 1") {
-                            val result = sampleRemap(pod.iteratorNext(iteratorKey1, 33))
-                                    ?.map { it.asInt() }
-
-                            it("should be the same as defined sequence 0th elements in triplets") {
-                                assertThat(result).isEqualTo(seq.windowed(3, 3, true).filter { it.size > 1 }.map { it[1] })
-                            }
-                        }
-                        describe("Partition 2") {
-                            val result = sampleRemap(pod.iteratorNext(iteratorKey2, 33))
-                                    ?.map { it.asInt() }
-
-                            it("should be the same as defined sequence 0th elements in triplets") {
-                                assertThat(result).isEqualTo(seq.windowed(3, 3, true).filter { it.size > 2 }.map { it[2] })
-                            }
-                        }
+                    val iteratorKeys by memoized(mode = CachingMode.SCOPE) {
+                        arrayOf(
+                                pod.iteratorStart(100.0f, 0),
+                                pod.iteratorStart(100.0f, 1),
+                                pod.iteratorStart(100.0f, 2)
+                        )
                     }
 
+                    it("Partition 0: should be the same as defined sequence 0th elements in triplets") {
+                        assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[0], 34))?.map { it.asInt() })
+                                .isEqualTo(seq.windowed(3, 3, true).map { it[0] })
+                    }
+
+                    it("Partition 1: should be the same as defined sequence 0th elements in triplets") {
+                        assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[1], 33))?.map { it.asInt() })
+                                .isEqualTo(seq.windowed(3, 3, true).filter { it.size > 1 }.map { it[1] })
+                    }
+
+                    it("Partition 2: should be the same as defined sequence 0th elements in triplets") {
+                        assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[2], 33))?.map { it.asInt() })
+                                .isEqualTo(seq.windowed(3, 3, true).filter { it.size > 2 }.map { it[2] })
+                    }
                 }
             }
 
             describe("Iterate over pod sequence with 2 proxies. Two partitions") {
-                newTestSplittingPod(seq, 2).use { pod ->
+                val pod by memoized(mode = CachingMode.SCOPE) { newTestSplittingPod(seq, 2) }
 
-                    val iteratorKey1p0 = pod.iteratorStart(100.0f, 0)
-                    val iteratorKey2p0 = pod.iteratorStart(100.0f, 0)
-                    val iteratorKey1p1 = pod.iteratorStart(100.0f, 1)
-                    val iteratorKey2p1 = pod.iteratorStart(100.0f, 1)
+                val iteratorKeys by memoized(mode = CachingMode.SCOPE) {
+                    arrayOf(
+                            pod.iteratorStart(100.0f, 0), pod.iteratorStart(100.0f, 0),
+                            pod.iteratorStart(100.0f, 1), pod.iteratorStart(100.0f, 1)
+                    )
+                }
 
-                    describe("Partition 0") {
-                        val result1 = sampleRemap(pod.iteratorNext(iteratorKey1p0, 50))
-                                ?.map { it.asInt() }
-                        val result2 = sampleRemap(pod.iteratorNext(iteratorKey2p0, 50))
-                                ?.map { it.asInt() }
+                it("Partition 0: first should be the same as defined sequence even elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[0], 50))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[0] })
+                }
+                it("Partition 0: second should be the same as defined sequence even elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[1], 50))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[0] })
+                }
 
-                        it("first should be the same as defined sequence even elements") {
-                            assertThat(result1).isEqualTo(seq.windowed(2, 2, true).map { it[0] })
-                        }
-                        it("second should be the same as defined sequence even elements") {
-                            assertThat(result2).isEqualTo(seq.windowed(2, 2, true).map { it[0] })
-                        }
-                    }
-
-                    describe("Partition 1") {
-                        val result1 = sampleRemap(pod.iteratorNext(iteratorKey1p1, 50))
-                                ?.map { it.asInt() }
-                        val result2 = sampleRemap(pod.iteratorNext(iteratorKey2p1, 50))
-                                ?.map { it.asInt() }
-
-                        it("first should be the same as defined sequence odd elements") {
-                            assertThat(result1).isEqualTo(seq.windowed(2, 2, true).map { it[1] })
-                        }
-                        it("second should be the same as defined sequence odd elements") {
-                            assertThat(result2).isEqualTo(seq.windowed(2, 2, true).map { it[1] })
-                        }
-                    }
+                it("Partition 1: first should be the same as defined sequence odd elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[2], 50))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[1] })
+                }
+                it("Partition 1: second should be the same as defined sequence odd elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[3], 50))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[1] })
                 }
             }
 
             describe("Iterate over pod sequence for more than defined in sequence at once. Two partitions") {
-                newTestSplittingPod(seq, 2).use { pod ->
+                val pod by memoized(mode = CachingMode.SCOPE) { newTestSplittingPod(seq, 2) }
 
-                    val iteratorKey0 = pod.iteratorStart(100.0f, 0)
-                    val iteratorKey1 = pod.iteratorStart(100.0f, 1)
-                    describe("Partition 0") {
-                        val e = sampleRemap(pod.iteratorNext(iteratorKey0, 51))
-                                ?.map { it.asInt() }
+                val iteratorKeys by memoized(mode = CachingMode.SCOPE) {
+                    arrayOf(pod.iteratorStart(100.0f, 0), pod.iteratorStart(100.0f, 1))
+                }
 
-                        it("should be the same as defined sequence odd elements") {
-                            assertThat(e).isEqualTo(seq.windowed(2, 2, true).map { it[0] })
-                        }
-                    }
+                it("Partition 0: should be the same as defined sequence odd elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[0], 51))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[0] })
+                }
 
-                    describe("Partition 1") {
-                        val e = sampleRemap(pod.iteratorNext(iteratorKey1, 51))
-                                ?.map { it.asInt() }
-
-                        it("should be the same as defined sequence even elements") {
-                            assertThat(e).isEqualTo(seq.windowed(2, 2, true).map { it[1] })
-                        }
-                    }
+                it("Partition 1: should be the same as defined sequence even elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[1], 51))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[1] })
                 }
             }
 
             describe("Iterate over pod sequence for more than defined in sequence in two attempts. Two partitions") {
-                newTestSplittingPod(seq, 2).use { pod ->
+                val pod by memoized(mode = CachingMode.SCOPE) { newTestSplittingPod(seq, 2) }
 
-                    val iteratorKey0 = pod.iteratorStart(100.0f, 0)
-                    val iteratorKey1 = pod.iteratorStart(100.0f, 1)
+                val iteratorKeys by memoized(mode = CachingMode.SCOPE) {
+                    arrayOf(pod.iteratorStart(100.0f, 0), pod.iteratorStart(100.0f, 1))
+                }
 
-                    describe("Partition 0") {
-                        val e1 = sampleRemap(pod.iteratorNext(iteratorKey0, 50))
-                                ?.map { it.asInt() }
-                        val e2 = sampleRemap(pod.iteratorNext(iteratorKey0, 1))
-                                ?.map { it.asInt() }
-
-                        it("first attempt should be the same as defined sequence even elements") {
-                            assertThat(e1).isEqualTo(seq.windowed(2, 2, true).map { it[0] })
-                        }
-                        it("second attempt should be null") {
-                            assertThat(e2).isNull()
-                        }
-                    }
-
-                    describe("Partition 1") {
-                        val e1 = sampleRemap(pod.iteratorNext(iteratorKey1, 50))
-                                ?.map { it.asInt() }
-                        val e2 = sampleRemap(pod.iteratorNext(iteratorKey1, 1))
-                                ?.map { it.asInt() }
-
-                        it("first attempt should be the same as defined sequence odd elements") {
-                            assertThat(e1).isEqualTo(seq.windowed(2, 2, true).map { it[1] })
-                        }
-                        it("second attempt should be null") {
-                            assertThat(e2).isNull()
-                        }
-                    }
+                it("Partition 0: first attempt should be the same as defined sequence even elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[0], 50))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[0] })
+                }
+                it("Partition 0: second attempt should be null") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[0], 1))?.map { it.asInt() })
+                            .isNull()
+                }
+                it("Partition 1: first attempt should be the same as defined sequence odd elements") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[1], 50))?.map { it.asInt() })
+                            .isEqualTo(seq.windowed(2, 2, true).map { it[1] })
+                }
+                it("Partition 1: second attempt should be null") {
+                    assertThat(sampleRemap(pod.iteratorNext(iteratorKeys[1], 1))?.map { it.asInt() })
+                            .isNull()
                 }
             }
         }
