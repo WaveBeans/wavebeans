2d88e8be32335f521ae93994d170aba43daffd61
asubb <a.a.subb@gmail.com>
2020-07-02 13:00:42 -0400

    [metric service][fail safe implementation of submitting metrics to connectors by the service]

--- exe/src/test/kotlin/io/wavebeans/execution/metrics/MetricServiceSpec.kt
+++ exe/src/test/kotlin/io/wavebeans/execution/metrics/MetricServiceSpec.kt
@@ -1,20 +1,24 @@
 package io.wavebeans.execution.metrics
 
-import com.nhaarman.mockitokotlin2.eq
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
+import assertk.assertThat
+import assertk.assertions.isNull
+import assertk.catch
+import com.nhaarman.mockitokotlin2.*
+import org.mockito.ArgumentMatchers
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.lifecycle.CachingMode
 import org.spekframework.spek2.style.specification.describe
 
 object MetricServiceSpec : Spek({
 
     describe("Mock implementation") {
 
-        val myMetricObject = MetricObject("io.wavebeans.execution.metrics", "test", emptyMap())
-        val myMetricConnector = mock<MetricConnector>()
+        val myMetricObject by memoized(CachingMode.TEST) { MetricObject("io.wavebeans.execution.metrics", "test", emptyMap()) }
+        val myMetricConnector by memoized(CachingMode.TEST) { mock<MetricConnector>() }
 
-        beforeGroup {
-            MetricServer.metricConnectors += myMetricConnector
+        beforeEachTest {
+            MetricService.reset()
+            MetricService.registerConnector(myMetricConnector)
         }
 
         it("should record increment") {
@@ -60,6 +64,48 @@ object MetricServiceSpec : Spek({
         it("should record time value 42") {
             myMetricObject.time(42)
             verify(myMetricConnector).time(eq(myMetricObject), eq(42L))
+        }
+
+        it("shouldn't fail if exception is thrown by one of the connectors during increment") {
+            whenever(myMetricConnector.increment(any(), any())).thenThrow(IllegalStateException("shouldn't throw it"))
+            assertThat(catch { myMetricObject.increment() }).isNull()
+            verify(myMetricConnector).increment(eq(myMetricObject), eq(1L))
+        }
+
+        it("shouldn't fail if exception is thrown by one of the connectors during decrement") {
+            whenever(myMetricConnector.decrement(any(), any())).thenThrow(IllegalStateException("shouldn't throw it"))
+            assertThat(catch { myMetricObject.decrement() }).isNull()
+            verify(myMetricConnector).decrement(eq(myMetricObject), eq(1L))
+        }
+
+        it("shouldn't fail if exception is thrown by one of the connectors during gauge record with long") {
+            whenever(myMetricConnector.gauge(any(), ArgumentMatchers.anyLong())).thenThrow(IllegalStateException("shouldn't throw it"))
+            assertThat(catch { myMetricObject.gauge(1L) }).isNull()
+            verify(myMetricConnector).gauge(eq(myMetricObject), eq(1L))
+        }
+
+        it("shouldn't fail if exception is thrown by one of the connectors during gauge record with double") {
+            whenever(myMetricConnector.gauge(any(), ArgumentMatchers.anyDouble())).thenThrow(IllegalStateException("shouldn't throw it"))
+            assertThat(catch { myMetricObject.gauge(1.0) }).isNull()
+            verify(myMetricConnector).gauge(eq(myMetricObject), eq(1.0))
+        }
+
+        it("shouldn't fail if exception is thrown by one of the connectors during gauge delta record with long") {
+            whenever(myMetricConnector.gaugeDelta(any(), ArgumentMatchers.anyLong())).thenThrow(IllegalStateException("shouldn't throw it"))
+            assertThat(catch { myMetricObject.gaugeDelta(1L) }).isNull()
+            verify(myMetricConnector).gaugeDelta(eq(myMetricObject), eq(1L))
+        }
+
+        it("shouldn't fail if exception is thrown by one of the connectors during gauge delta record with double") {
+            whenever(myMetricConnector.gaugeDelta(any(), ArgumentMatchers.anyDouble())).thenThrow(IllegalStateException("shouldn't throw it"))
+            assertThat(catch { myMetricObject.gaugeDelta(1.0) }).isNull()
+            verify(myMetricConnector).gaugeDelta(eq(myMetricObject), eq(1.0))
+        }
+
+        it("shouldn't fail if exception is thrown by one of the connectors during time record") {
+            whenever(myMetricConnector.time(any(), any())).thenThrow(IllegalStateException("shouldn't throw it"))
+            assertThat(catch { myMetricObject.time(1L) }).isNull()
+            verify(myMetricConnector).time(eq(myMetricObject), eq(1L))
         }
     }
 })
