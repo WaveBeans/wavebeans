b45a92f0c24fbe378e6da1f38d3c2df1c049475b
asubb <a.a.subb@gmail.com>
2020-10-11 15:02:19 -0400

    [metric-server][lightened dependency on `proto` project]

--- metrics/core/build.gradle.kts
+++ metrics/core/build.gradle.kts
@@ -3,5 +3,10 @@ tasks.jar {
 }
 
 dependencies {
-    implementation(project(":proto"))
+    compileOnly(project(":proto")) {
+        because("`lib` is dependent on this project but `proto` is required for " +
+                "distributed execution only which will provide the `proto` implicitly")
+    }
+
+    testImplementation(project(":proto"))
 }
