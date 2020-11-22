fc3c6cb761086fef423c924f75ff0d62808b77de
asubb <a.a.subb@gmail.com>
2020-11-22 11:17:38 -0500

    [tests][dependency on filesystems project]

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     implementation(project(":exe"))
     implementation(project(":proto"))
     implementation(project(":metrics:core"))
+    implementation(project(":filesystems:core"))
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("com.willowtreeapps.assertk:assertk-jvm:0.13")
 }
