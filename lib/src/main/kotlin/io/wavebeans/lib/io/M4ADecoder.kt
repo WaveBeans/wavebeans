67d2b20a1a406a2d68a887b26387b9d291e210ec
asubbotin <asubbotin@snaplogic.com>
2022-05-11 20:09:27 -0400

    [M4A format] Boxes abstraction

--- lib/src/main/kotlin/io/wavebeans/lib/io/M4ADecoder.kt
+++ lib/src/main/kotlin/io/wavebeans/lib/io/M4ADecoder.kt
@@ -1,19 +1,21 @@
 package io.wavebeans.lib.io
 
-import io.wavebeans.lib.io.BlockFormat.mdat
-import io.wavebeans.lib.io.BlockFormat.unknown
+import io.wavebeans.lib.io.BoxType.*
 import java.io.DataInputStream
 import java.io.File
 
 class M4ADecoder {
 }
 
-enum class BlockFormat(val int: Int) {
-    unknown(0),
-    ftyp(0x66747970),
-    mdat(0x6D646174),
-    moov(0x6D6F6F76),
-    free(0x66726565)
+enum class BoxType(val int: Int, val createBox: (Int, DataInputStream) -> Box) {
+    root(0, { size, stream -> RootBox(stream) }),
+    unknown(
+        -1,
+        { _, _ -> throw kotlin.UnsupportedOperationException("unknown box type can't be created") }),
+    ftyp(0x66747970, { size, stream -> FtypeBox(size, stream) }),
+    mdat(0x6D646174, { size, stream -> MdatBox(size, stream) }),
+    moov(0x6D6F6F76, { size, stream -> MoovBox(size, stream) }),
+    free(0x66726565, { size, stream -> FreeBox(size, stream) })
     ;
 
     companion object {
@@ -23,7 +25,7 @@ enum class BlockFormat(val int: Int) {
                 .joinToString("")
         }
 
-        fun of(int: Int): BlockFormat? {
+        fun of(int: Int): BoxType? {
             return values().firstOrNull { it.int == int }
         }
     }
@@ -34,31 +36,68 @@ enum class BlockFormat(val int: Int) {
 // http://www.telemidia.puc-rio.br/~rafaeldiniz/public_files/normas/ISO-14496/ISO_IEC_14496-1_2004(E).pdf
 // https://mp4ra.org/
 fun main(args: Array<String>) {
-    val f = File("/users/asubb/Downloads/Untitled.m4a")
-    val blocks = ArrayList<Block>()
+    val f = File("${System.getProperty("user.home")}/Downloads/Untitled.m4a")
     DataInputStream(f.inputStream().buffered()).use {
-        do {
-            val size = it.readBEInt() ?: break
-            val type = it.readType() ?: break
-
-            if (size == 1 && type == mdat) {
-                TODO("handle large size")
-            }
-
-            val buf = ByteArray(size - 8)
-            val readBytes = it.read(buf)
-            if (readBytes > 0) {
-                blocks += Block(type, buf)
-            }
-            println(
-                "size=$size type=$type buf=\n${toHexString(buf)}"
-            )
-        } while (readBytes > 0)
-
+        RootBox(it).read()
     }
 }
 
-private fun toHexString(buf: ByteArray): String = buf.mapIndexed { idx, byte ->
+abstract class Box(val size: Int, val type: BoxType, val inputStream: DataInputStream) {
+    abstract fun read()
+}
+
+class RootBox(inputStream: DataInputStream) : Box(0, root, inputStream) {
+
+    val children = ArrayList<Box>()
+
+    override fun read() {
+        do {
+            val size = inputStream.readBEInt() ?: break
+            val type = inputStream.readType() ?: break
+
+            if (size == 1 && type == mdat) {
+                TODO("Handle large size content")
+            }
+
+            if (type != unknown) {
+                children += type.createBox(size - 8, inputStream).also { it.read() }
+            } else {
+                val buf = inputStream.readBuffer(size - 8)
+                println("Read $size bytes of type $type:\n${buf.toHexString()}")
+            }
+        } while (inputStream.available() > 0)
+    }
+}
+
+class FtypeBox(size: Int, stream: DataInputStream) : Box(size, ftyp, stream) {
+    override fun read() {
+        val buf = inputStream.readBuffer(size)
+        println("Read $size bytes of type $type:\n${buf.toHexString()}")
+    }
+}
+
+class MdatBox(size: Int, stream: DataInputStream) : Box(size, mdat, stream) {
+    override fun read() {
+        val buf = inputStream.readBuffer(size)
+        println("Read $size bytes of type $type:\n${buf.toHexString()}")
+    }
+}
+
+class MoovBox(size: Int, stream: DataInputStream) : Box(size, moov, stream) {
+    override fun read() {
+        val buf = inputStream.readBuffer(size)
+        println("Read $size bytes of type $type:\n${buf.toHexString()}")
+    }
+}
+
+class FreeBox(size: Int, stream: DataInputStream) : Box(size, free, stream) {
+    override fun read() {
+        val buf = inputStream.readBuffer(size)
+        println("Read $size bytes of type $type:\n${buf.toHexString()}")
+    }
+}
+
+private fun ByteArray.toHexString(): String = mapIndexed { idx, byte ->
     (byte.toInt() and 0xFF).let {
         if (it in 0x30..0x7f)
             it.toChar().toString().padStart(2, ' ')
@@ -66,19 +105,26 @@ private fun toHexString(buf: ByteArray): String = buf.mapIndexed { idx, byte ->
     } + if ((idx + 1) % 16 == 0) "\n" else " "
 }.joinToString("")
 
-fun DataInputStream.readType(): BlockFormat? {
+private fun DataInputStream.readBuffer(size: Int): ByteArray {
+    val buf = ByteArray(size)
+    val bytesRead = this.read(buf)
+    require(bytesRead == size) { "Expected to read $size bytes but read $bytesRead" }
+    return buf
+}
+
+private fun DataInputStream.readType(): BoxType? {
     if (available() < 4) return null
     val typeData = readInt()
-    val type = BlockFormat.of(typeData)
+    val type = BoxType.of(typeData)
     return if (type == null) {
-        println("Type 0x${typeData.toString(16)} (${BlockFormat.decode(typeData)}) is not recognized")
+        println("Type 0x${typeData.toString(16)} (${BoxType.decode(typeData)}) is not recognized")
         unknown
     } else {
         type
     }
 }
 
-fun DataInputStream.readBEInt(): Int? {
+private fun DataInputStream.readBEInt(): Int? {
     if (available() < 4) return null
     var acc = 0
     for (i in 0..3) {
@@ -89,6 +135,6 @@ fun DataInputStream.readBEInt(): Int? {
     return acc
 }
 
-class Block(val type: BlockFormat, val buf: ByteArray) {
+class IBlock(val type: BoxType, val buf: ByteArray) {
 
 }
